082626c3501592340ef4ed042050e9e83a0d1a61
Siddharth-104 <72357637+Siddharth-104@users.noreply.github.com>
2020-12-15 19:07:16 +0530

    Update main.c
    Updated main.c for release  from my end.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,27 @@
-#include <stdio.h>
+#include "snake.h"
 
 int main(void){
 	return 0;
+}
+
+void Wall(void)
+{
+   system("cls");
+   int i;
+   GotoXY(food.x,food.y); 
+       printf("O");
+   for(i=GA_XL;i<=GA_XR;i++)
+   {
+       GotoXY(i,GA_YT);
+           printf("%c",176);
+       GotoXY(i,GA_YB);
+           printf("%c",176);
+   }
+   for(i=GA_YT+1;i<=GA_YB;i++)
+   {
+       GotoXY(GA_XL,i);
+           printf("%c",176);
+       GotoXY(GA_XR,i);
+       printf("%c",176);
+   }
 }
