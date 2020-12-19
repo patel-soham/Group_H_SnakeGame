be1a3c5003d19b6db765353bf5be5596c6235348
Soham Patel <40416883+patel-soham@users.noreply.github.com>
2020-12-19 18:55:43 +0530

    Update snake.h

--- include/snake.h
+++ include/snake.h
@@ -104,14 +104,16 @@ void snake(void);
 
 /**  @brief Function prototype to move the cursor to specific position
 *
-* This Function places cursor at given coordinate position and takes Input parameters as x-coordinate and y-coordinate
+* This Function places cursor at given coordinate position 
+* and takes Input parameters as x-coordinate and y-coordinate
 *
 */
 void gotoxy(int,int);
 
 /**  @brief Function prototype to move the cursor to specific position
 *
-* This Function places cursor at given coordinate position and takes Input parameters as x-coordinate and y-coordinate
+* This Function places cursor at given coordinate position 
+* and takes Input parameters as x-coordinate and y-coordinate
 *
 */
 void goto_xy(int,int);
